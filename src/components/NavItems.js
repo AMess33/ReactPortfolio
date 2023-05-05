52b8293c30146122b35eabe16e9c5b8d61c5c358
Andrew M <110314379+AMess33@users.noreply.github.com>
2023-05-05 14:05:41 -0500

    Add files via upload

---
+++ src/components/NavItems.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function NavItems({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#About"
+          onClick={() => handlePageChange("About")}
+          className={currentPage === "About" ? "nav-link active" : "nav-link"}
+        >
+          About Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Portfolio"
+          onClick={() => handlePageChange("Portfolio")}
+          className={
+            currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+          }
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Contact"
+          onClick={() => handlePageChange("Contact")}
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Resume"
+          onClick={() => handlePageChange("Resume")}
+          className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavItems;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import NavItems from "./NavItems";
+import Contact from "./Contact Form/Contact";
+import Portfolio from "./Portfolio";
+import Resume from "./Resume";
+import About from "./About";
+import background from "../images/ContentContainer.jpg";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("");
+
+  const renderPage = () => {
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <div>
+        <NavItems
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}
+        />
+      </div>
+      <div
+        className="content"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+        }}
+      >
+        {renderPage()}
+      </div>
+    </div>
+  );
+}
